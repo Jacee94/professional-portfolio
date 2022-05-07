ab41f9d0aacf342b7a4deca1a9e3038eec59c5e1
jace.e94 <jace.edwards94@gmail.com>
2022-05-06 21:57:10 -0600

    css updates and navigation dropdown animation

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,9 +1,26 @@
+var shortcutClicked = false;
+
+// Scroll animation to clicked section
 $(".header-shortcut").on("click", function(){
     var scrollElId = $(this).html();
     scrollElId = scrollElId.toLowerCase();
     scrollElId = scrollElId.replace(" ", "-");
     
-    $([document.documentElement, document.body]).animate({
-        scrollTop: $("#" + scrollElId).offset().top - $(".section-header").outerHeight()
-    }, 1000);
+    // Don't run if in the middle of an animation
+    if(!shortcutClicked){
+        $([document.documentElement, document.body]).animate({
+            scrollTop: $("#" + scrollElId).offset().top - $(".section-header").outerHeight()
+        }, 1000);
+        shortcutClicked = true;
+
+        setTimeout(function(){
+            shortcutClicked = false;
+        }, 1000);
+    }
+});
+
+$('.dropdown').hover(function() {
+    $(this).find('.dropdown-menu').first().stop(true, true).slideDown();
+}, function() {
+    $(this).find('.dropdown-menu').first().stop(true, true).delay(100).slideUp()
 });
