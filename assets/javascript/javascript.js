bab358e14dccb39a7c7f068bdf034c264a2944fa
jace.e94 <jace.edwards94@gmail.com>
2022-05-07 01:12:03 -0600

    api retrieval progress

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,3 +1,68 @@
+var repoFavorites = [];
+var repoImageUrls = [];
+
+var getUserRepos = function(user) {
+    // format the github api url
+    var apiUrl = "https://api.github.com/users/jacee94/repos";
+  
+    // make a request to the url
+    fetch(apiUrl).then(function(response) {
+        // request was successful
+        if (response.ok) {
+            response.json().then(function(data) {
+                checkRepoFavorites(data);
+            });
+        } else {
+            alert('Error: GitHub User Not Found');
+        }
+    })
+}
+
+// Iterate through repositories looking for favorites
+function checkRepoFavorites(repoData){
+    for(var i = 0; i < repoData.length; i++){
+        var fav = repoData[i].topics[0];
+        if(fav){
+            if(fav == "favorite"){
+                var userObj = {
+                    login: repoData[i].owner.login,
+                    repo: repoData[i].name
+                }
+                repoFavorites.push(userObj);
+            }
+            
+        }
+    }
+
+    getReposInfo();
+}
+
+function getReposInfo(){
+    // format the github api url
+    for(var i = 0; i < repoFavorites.length; i++){
+        var apiUrl = "https://api.github.com/repos/" + repoFavorites[i].login + "/" + repoFavorites[i].repo +"/contents/assets/images";
+        repoImageUrls.push(apiUrl);
+    }
+
+    console.log(repoImageUrls);
+
+    // Make a request to the url
+    fetch(repoImageUrls[0]).then(function(response) {
+        // request was successful
+        if (response.ok) {
+            response.json().then(function(data) {
+                console.log(data);
+            });
+        } else {
+            alert('Error: GitHub REPO not found');
+        }
+    })
+}
+
+getUserRepos();
+
+
+
 var shortcutClicked = false;
 
 // Scroll animation to clicked section
@@ -18,6 +83,8 @@ $(".header-shortcut").on("click", function(){
         }, 750);
     }
 });
+
+// Drop down navigation functionality
 
 $(window).resize(function(){
     $(".dropdown-menu").width($("#dropdownMenuButton").outerWidth() + "px");
